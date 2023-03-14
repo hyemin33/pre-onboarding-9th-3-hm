3b28807d0c9c176306df2a3aaff6f807ea4a6f05
hyemin33 <joliie@naver.com>
2023-03-14 16:03:59 +0900

    feat : data setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { useState, useEffect } from 'react';
+import { api } from './apis/api';
+
 function App() {
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    await api
+      .get('')
+      .then((res) => {
+        setData(res.data.response);
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(data);
+
   return (
     <div className='App'>
-      <h1>Vite + React</h1>
+      <h1>시계열 차트 만들기</h1>
     </div>
   );
 }
